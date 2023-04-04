108737d31a07d319d6311cdc5a80ff9e2b33418c
Mike Ralphson <mike.ralphson@gmail.com>
2023-04-04 10:39:12 +0100

    Add training data, allow prompt on cmdline

---
+++ README.md
@@ -0,0 +1,4 @@
+## TODO
+
+* convert training data from html to markdown
+* create embeddings or finetune it
--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function main() {
-  const prompt = "According to Douglas Adams, what is the answer to the meaning of life, the universe, and everything?";
+  let prompt = "According to Douglas Adams, what is the answer to the meaning of life, the universe, and everything?";
+  if (process.argv.length > 2) {
+    prompt = Array(process.argv).shift(2,0).join(' ');
+  }
   const temperature = 0.5;
   const top_p = 1.0;
   const n = 3;
 
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
+    max_tokens: 750,
     prompt,
   });
   console.log(`${prompt}:> ${completion.data.choices[0].text}`);
