3a5a0fc59adf77ba08e78112d2cf7f40680a8981
Mike Ralphson <mike.ralphson@gmail.com>
2023-04-11 07:10:01 +0100

    build: update deps and docs

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
+# Vector-Victor
+
+## SQL tables needed
+
+* See `table-ops.sql`
+
+## Included scripts
+
+* convert training data from html to markdown (`html2md.js`)
+* create embeddings using OpenAI API (`import1.js`)
+* cache queries and search by cosine affinity (`query1.js`)
+* link queries to prompts to correct errors (`link1.js`)
+
 ## TODO
 
-* convert training data from html to markdown
-* create embeddings or finetune it
+* Allow to run with `sqlite-vss` as well as Postgres and `pgvector` extension
--- html2md.js
+++ html2md.js
@@ -1,14 +1,11 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('node:fs');
 const TurndownService = require('turndown');
 const turndownService = new TurndownService();
 
 const s = fs.readFileSync(process.argv[2],'utf8').split('\r').join('');
-const a = s.split('<');
-const x = a.shift(1);
-const s2 = '<'+a.join('<').replace('HTML','html');
 
-const markdown = turndownService.turndown(s2);
-fs.writeFileSync(process.argv[2].replace('.htm','.md'),markdown,'utf8');
+const markdown = turndownService.turndown(s);
+fs.writeFileSync(process.argv[2].replace('.html','.htm').replace('.htm','.md'),markdown,'utf8');
 
